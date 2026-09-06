fa5fd4d2cfc84347182a5e8b6a7deb3a3cb5f61f
bajpaianushka <anushka.39934@stu.upes.ac.in>
2026-09-06 23:21:52 +0530

    Add files via upload

---
+++ q22.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	float cp = 1000;
+	float sp = 1200;
+	scanf("%f %f", &cp, &sp);
+
+	if(sp > cp){
+		float profit_percent = (sp - cp) / cp * 100;
+		printf("Profit %.0f%%", profit_percent);
+	}
+	else if(cp > sp){
+		float loss_percent = (cp - sp) / cp * 100;
+		printf("Loss %.0f%%", loss_percent);
+	}
+	else{
+		printf("No Profit No Loss");
+	}
+
+	return 0;
+}
